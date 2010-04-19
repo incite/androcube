faeb3af5db63ccb7779c056c24825becbed36e86
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-04-19 17:08:11 +1000

    box cat and gitignore apk

--- androcube/src/com/incite/toolbox/InciteToolbox.java
+++ androcube/src/com/incite/toolbox/InciteToolbox.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class InciteToolbox extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+    
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	
 }
