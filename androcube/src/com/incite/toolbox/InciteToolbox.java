f0f259c6b6d1f0be6429c2178dd1085a353723b7
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-04-20 15:40:20 +1000

    login screen

--- androcube/src/com/incite/toolbox/InciteToolbox.java
+++ androcube/src/com/incite/toolbox/InciteToolbox.java
@@ -2,12 +2,26 @@ package com.incite.toolbox;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class InciteToolbox extends Activity {
+public class InciteToolbox extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
+        
+        Button button = (Button)findViewById(R.id.login);
+        button.setOnClickListener(this);
+
     }
+    
+    public void onClick(View v) {
+		
+        setContentView(R.layout.main);
+    	
+	}
 }
